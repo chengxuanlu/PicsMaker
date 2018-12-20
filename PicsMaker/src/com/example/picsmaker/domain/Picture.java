9f523edd8bdf6e24c198873936b2f572de7935e5
chengxuanlu <xuanlucheng@gmail.com>
2018-12-20 19:57:30 +0800

    Picture类新增了构造函数和get方法

--- PicsMaker/src/com/example/picsmaker/domain/Picture.java
+++ PicsMaker/src/com/example/picsmaker/domain/Picture.java
@@ -1,12 +1,21 @@
 
 package com.example.picsmaker.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Picture {
 	private String path;
 	private List<String> tags;
 
+	Picture(String p, String tag){
+		this.path = p;
+		
+		tags = new ArrayList<String>();
+		tags.add(tag);
+	}
+	
+	
 	public String getPath() {
 		return path;
 	}
